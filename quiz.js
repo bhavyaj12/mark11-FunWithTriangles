f6ac3e8c548ab7699703d76b3a856a626b68eb4a
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2021-09-22 02:04:49 +0530

    feat: added quiz

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnsBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output");
+
+const ansKey = ["isosceles", "one obtuse angle", "right triangle", "Yes", "Yes", "25°", "120°", "scalene", "obtuse", "30, 45"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()) {
+        if(value === ansKey[index]) {
+            score = score + 1;
+        }
+        index = index + 1; 
+    }
+                                
+    outputBox.innerText = "Quiz over! Your final score is: " + score;
+}
+
+submitAnsBtn.addEventListener("click", calculateScore);
